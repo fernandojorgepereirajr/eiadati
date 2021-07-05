dbde8785fd8ff01e7950a8ac4d7b9c340df0f778
fernandojorgepereirajr <fernando.junior@ccc.ufcg.edu.br>
2021-07-05 19:46:30 -0300

    Initial settings

---
+++ backend/src/main/java/com/example/eiadati/EiadatiApplication.java
@@ -0,0 +1,12 @@
+package com.example.eiadati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EiadatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EiadatiApplication.class, args);
+	}
+}
